3d21eea2347cc382b46ddc08de29b19ec6e88231
Geoffroy Empain <geoffroy@charlie-bravo.be>
2020-12-18 17:04:12 +0100

    fix: tar not working for branches with slashes

--- src/commands/upload/upload.ts
+++ src/commands/upload/upload.ts
@@ -4,11 +4,12 @@ import { Logger } from '../../commons/logger/logger';
 import { UploadOptions } from './upload-options';
 import { uploadArchive } from './upload-archive';
 import { archiveFiles } from './archive-files';
+import { v4 as uuid } from 'uuid';
 
 const logger = new Logger('meli.cli:upload');
 
 export async function upload(options: UploadOptions): Promise<void> {
-  const archivePath = join(tmpdir(), `${options.site}-${options.release ?? 'latest'}-${options.branch}.tar.gz`);
+  const archivePath = join(tmpdir(), `site-${uuid()}.tar.gz`);
 
   logger.info(`Compressing files from ${options.directory}....`);
   await archiveFiles(options.directory, archivePath);
